4c9d3786f68a4d432e09284d4330ad23db5875de
DanaKhromenko <danadiadius@gmail.com>
2023-09-10 15:17:40 +0300

    Executable files

---
+++ desktop/Export/windows/haxe/ManifestResources.hx
@@ -0,0 +1,160 @@
+package;
+
+import haxe.io.Bytes;
+import lime.utils.AssetBundle;
+import lime.utils.AssetLibrary;
+import lime.utils.AssetManifest;
+import lime.utils.Assets;
+
+#if sys
+import sys.FileSystem;
+#end
+
+#if disable_preloader_assets
+@:dox(hide) class ManifestResources {
+	public static var preloadLibraries:Array<Dynamic>;
+	public static var preloadLibraryNames:Array<String>;
+	public static var rootPath:String;
+
+	public static function init (config:Dynamic):Void {
+		preloadLibraries = new Array ();
+		preloadLibraryNames = new Array ();
+	}
+}
+#else
+@:access(lime.utils.Assets)
+
+
+@:keep @:dox(hide) class ManifestResources {
+
+
+	public static var preloadLibraries:Array<AssetLibrary>;
+	public static var preloadLibraryNames:Array<String>;
+	public static var rootPath:String;
+
+
+	public static function init (config:Dynamic):Void {
+
+		preloadLibraries = new Array ();
+		preloadLibraryNames = new Array ();
+
+		rootPath = null;
+
+		if (config != null && Reflect.hasField (config, "rootPath")) {
+
+			rootPath = Reflect.field (config, "rootPath");
+
+			if(!StringTools.endsWith (rootPath, "/")) {
+
+				rootPath += "/";
+
+			}
+
+		}
+
+		if (rootPath == null) {
+
+			#if (ios || tvos || emscripten)
+			rootPath = "assets/";
+			#elseif android
+			rootPath = "";
+			#elseif console
+			rootPath = lime.system.System.applicationDirectory;
+			#else
+			rootPath = "./";
+			#end
+
+		}
+
+		#if (openfl && !flash && !display)
+		
+		#end
+
+		var data, manifest, library, bundle;
+
+		#if kha
+
+		null
+		library = AssetLibrary.fromManifest (manifest);
+		Assets.registerLibrary ("null", library);
+
+		if (library != null) preloadLibraries.push (library);
+		else preloadLibraryNames.push ("null");
+
+		#else
+
+		Assets.libraryPaths["default"] = rootPath + "manifest/default.json";
+		
+
+		library = Assets.getLibrary ("default");
+		if (library != null) preloadLibraries.push (library);
+		else preloadLibraryNames.push ("default");
+		
+
+		#end
+
+	}
+
+
+}
+
+
+#if kha
+
+null
+
+#else
+
+#if !display
+#if flash
+
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_companies_companies_xml extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_companies_tivo_png extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_companies_tpe_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_desperate_housewives_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_dr_house_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_gravity_falls_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_hell_kitchen_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_insidious_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_leon_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_matilda_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_mortal_combat_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_phineas_and_ferb_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_pulp_fiction_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_shakespeare_in_love_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_shining_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_snatch_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_the_hobbit_an_unexpected_journey_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_the_hobbit_the_battle_of_the_five_armies_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_shows_the_hobbit_the_desolation_of_smaug_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_img_wallpapers_jpg extends flash.display.BitmapData { public function new () { super (0, 0, true, 0); } }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_shows_shows_txt extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__assets_shows_shows_xml extends null { }
+@:keep @:bind @:noCompletion #if display private #end class __ASSET__manifest_default_json extends null { }
+
+
+#elseif (desktop || cpp)
+
+
+
+
+#else
+
+
+
+#end
+
+#if (openfl && !flash)
+
+#if html5
+
+#else
+
+#end
+
+#end
+#end
+
+#end
+
+#end
